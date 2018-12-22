a33ecaab1f22b77f0d5b184595cbfcbc075f6300
R1zzo23 <adamjrizzo@yahoo.com>
2018-12-21 19:50:56 -0500

    added navbar, styles, components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { Header } from './shared/Header';
+import { RentalList } from './components/rental/RentalList';
 import logo from './logo.svg';
 import './App.css';
 
@@ -7,8 +8,10 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-      <Header />
-        I am APP COMPONENT
+        <Header />
+        <div className="container">
+          <RentalList />
+        </div>
       </div>
     );
   }
